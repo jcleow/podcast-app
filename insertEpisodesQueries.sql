b3cbb3c4338dfbafbf586de1a15386ad15888713
jcleow <jc.leow94@gmail.com>
2020-12-11 21:51:12 +0800

    Added some seed podcast_episodes queries for insertion

---
+++ insertEpisodesQueries.sql
@@ -0,0 +1,93 @@
+--Insertion of Podcast Episodes
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'Under Pressure with Musician Leisure McCorkle',
+  'Musician Leisure McCorkle (5000 Light Years Beyond the Speed of Sound) got the opportunity to play at the world famous Cavern Club, but there was a catch- he had to be back to Belfast by 9:00am for a seminar for his PhD. Listen to this rock n'' roll story that involves Liverpool, Anthropology and Noel Gallagher of the band Oasis.',null,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/685820626&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  1 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'Poker Stories with Actor Vince Van Patten',
+  'Actor Vince Van Patten (7 Days To Vegas, World Poker Tour) talks about growing up with his dad, actor Dick Van Patten, his love of poker, and his new movie 7 Days To Vegas.',null,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/682149206&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  1 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'Life with Actor Ed Asner',
+  'Actor Ed Asner (Mary Tyler Moore, Lou Grant, Up, Elf, The Grouchy Historian, Dead To Me, Briarpatch) opens up about his life as an actor, a political activist, a father, and the owner of cats and birds.',
+  null,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/678684849&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  1 
+);
+
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'That Time I Spent In A Convent with Comedian Lisa Orkin',
+  'Comedian Lisa Orkin (Project Woo Woo, Honestly Lisa) needed to get away from it all so she decided to spend some time at a convent.',
+  null,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/675356864&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  1 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'Turning 16 In Paris with Actress Mary Birdsong',
+  'Actress Mary Birdsong (The Descendants, Reno 911) was turning 16 and desperately wanted to go to Paris with her French class, but her family couldn’t afford it, so her mother hit the blackjack tables in Atlantic City.',
+  null,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/671325932&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  1 
+);
+
+
+-- id=2
+-- https://soundcloud.com/criminalshow
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'Photo, Hair, Fingerprint',
+  'In 1988, a man in Hickory, NC was sentenced to life in prison based on evidence that experts would later call "junk science." It took him 24 years to convince someone to look at the evidence again.',
+  80,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/363307148&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  2 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'Secrets and Séances',
+  'Helen Duncan was a famous medium who travelled around Britain in the 1940s performing séances. She claimed to speak to the dead, and even produce physical manifestations of their spirits. But when she seemed to know wartime secrets about the whereabouts of military ships, she caught the attention of MI5. And in 1944, a few months before D-day, she was convicted under a 200-year-old "Witchcraft Act".',
+  79,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/356824517&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  2 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'The Botanist',
+  'In 1993, a man in Steamboat Springs, Colorado was found dead in his home. He''d been burned with a stun gun, hit with a shovel, and shot several times. The victim''s wife was the primary suspect, but she had an alibi for the estimated time of death. Investigators were at a loss, and turned to two very unlikely people for help.',
+  78,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/350326810&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  2 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'The Escape',
+  'In 1962, two men managed to escape the one prison in America that was supposed to be inescapable. They were never found. More than 50 years later, their 82-year-old sister is still waiting for them to come home...and one U.S. Marshal is still on the case.',
+  77,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/347722095&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+  2 
+);
+
+INSERT INTO podcast_episodes(name,description,episode_number,podcast_ext_url,podcast_series_id)
+VALUES (
+  'The Big Lick',
+  'The Tennessee Walking Horse has a natural gait that''s famously smooth. And, if trained in a certain way, it can perform a walk that''s even more spectacular. But, there''s a secret behind how, exactly, these horses are trained to do the crowd-pleasing step they''re celebrated for. Mary Helen Montgomery brings us the story.'
+,
+  76,
+  'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/345526603&color=%23310b09&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true'
+  2 
+);
